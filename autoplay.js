5b989348df42c3b559c7c6c2729a9a5c43a4dff3
huhua <41320113+appleguardu@users.noreply.github.com>
2019-06-23 18:12:57 +0800

    轮播图组件

---
+++ autoplay.js
@@ -0,0 +1,66 @@
+export const autoplay = {
+    props: {
+        
+        autoplay: {
+            type: Boolean,
+            default: false
+        },
+        
+        autoplayTimeout: {
+            type: Number,
+            default: 2000
+        },
+       
+        autoplayHoverPause: {
+            type: Boolean,
+            default: true
+        }
+    },
+    data () {
+        return {
+            autoplayInterval: null
+        }
+    },
+    destroyed () {
+        this.pauseAutoplay()
+
+        if (!this.$isServer) {
+            this.$el.removeEventListener('mouseenter', this.pauseAutoplay)
+            this.$el.removeEventListener('mouseleave', this.startAutoplay)
+        }
+    },
+    methods: {
+        pauseAutoplay () {
+            if (this.autoplayInterval) {
+                this.autoplayInterval = clearInterval(this.autoplayInterval)
+            }
+        },
+        startAutoplay () {
+            if (this.autoplay) {
+                this.autoplayInterval = setInterval(() => {
+                    this.dir === 'ltr' ? this.goPrev() : this.goNext()
+                }, this.autoplayTimeout)
+            }
+        }
+    },
+    mounted () {
+        if (!this.$isServer && this.autoplayHoverPause) {
+            this.$el.addEventListener('mouseenter', this.pauseAutoplay)
+            this.$el.addEventListener('mouseleave', this.startAutoplay)
+        }
+
+        this.startAutoplay()
+    },
+    watch: {
+        autoplay(value) {
+            if (value) {
+                this.startAutoplay()
+            } else {
+                this.pauseAutoplay()
+            }
+
+        }
+    }
+}
+
+
